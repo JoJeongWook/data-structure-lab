c3847005e53cc2642d64ec9f95d4ca0c47733b92
JoJeongWook <whwjddnr009@gmail.com>
2023-04-17 11:58:23 +0900

    PetAndMusic

--- Chap02_Array_Class/Chap02_Class_Example1/class_polynomial.cpp
+++ Chap02_Array_Class/Chap02_Class_Example1/class_polynomial.cpp
@@ -2,6 +2,8 @@
 #include <cstring>
 #include "rectangle.h"
 #include "smarthome.h"
+#include "Pet.h"
+#include "music.h"
 
 void swap(int& x, int& y) {
 	int tmp;
@@ -38,10 +40,45 @@ int main() {
 	printf("rectangle 1 Area : %lf\n", r1.getArea());
 	std::cout << "rectangle 1 Area : " << r1.getArea() << std::endl;
 	std::cout << "Is square? " << r1.isSquare() << std::endl;
-	*/
+	
 
 	SmartHome myHome("ȫ�浿", 23, 40, true);
 	myHome.printStatus();
+	
 
+	Pet myPet("lucky", a, "dog");
+	cout << "my pet name is" << myPet.getName() << endl;
+	cout << "my pet age is" << myPet.getAge() << endl;
+	*/
+
+	MusicStreamingService myService("MyMusic");
+	myService.addMusic("OMG", "NewJeans", "Album", 2023);
+	myService.addMusic("Ditto", "NewJeans", "Album", 2023);
+	myService.addMusic("VIBE", "Taeyang", "Album", 2023);
+	myService.addMusic("ShutDown", "BlackPink", "BornPink", 2022);
+
+	cout << "enter the music title : ";
+	cin >> music_title;
+	Music* result = myService.searchbyTitle(music_title);
+	if (result != NULL) {
+		cout << "found: " << result->getTitle() << "by" << result->getArtist() << endl;
+	}
+
+	string artist_name;
+	cout << "enter the artist name : ";
+	cin >> artist_name;
+	vector<music*>artiseResult = myService.searchbyArtist(artist_name);
+	if (artistResult.size() > 0) {
+		cout << "found" << artistResult.size() << "songs by" << artist_name << endl;
+		for (int i = 0; i < artistResult.size(); i++) {
+			cout << artistResult[i]->getTitle() << endl;
+		}
+
+
+
+	}
+	else {
+		cout << "not found" << endl;
+	}
 	return 0;
 }
